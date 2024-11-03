57fe789e1ee2565cc23d01eb446c8f5cd85bfcba
Aleksandrov Aleksey <aaaleksandrov09@gmail.com>
2024-11-03 19:49:34 +0300

    add 7 pages

--- members/urls.py
+++ members/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
     path('members/', views.members, name='members'),
+    path('members/ask/', views.ask, name='ask'),
+    path('members/question/1/', views.question, name='question'),
+    path('members/tag/bender/', views.bender, name='bender'),
+    path('members/settings/', views.settings, name='settings'),
+    path('members/login/', views.login, name='login'),
+    path('members/register/', views.signup, name='sighUp'),
 ]
--- members/views.py
+++ members/views.py
@@ -6,3 +6,27 @@ from django.shortcuts import render
 def members(request):
   template = loader.get_template('base.html')
   return HttpResponse(template.render())
+
+def ask(request):
+  template = loader.get_template('ask.html')
+  return HttpResponse(template.render())
+
+def question(request):
+  template = loader.get_template('question.html')
+  return HttpResponse(template.render())
+
+def bender(request):
+  template = loader.get_template('bender.html')
+  return HttpResponse(template.render())
+
+def settings(request):
+  template = loader.get_template('settings.html')
+  return HttpResponse(template.render())
+
+def login(request):
+  template = loader.get_template('login.html')
+  return HttpResponse(template.render())
+
+def signup(request):
+  template = loader.get_template('sign up.html')
+  return HttpResponse(template.render())
